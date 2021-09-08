be5726fa0321f777aeb7c497ecf9b1b37912853b
laila503 <laila@LAPTOP-AGKUJDPT.localdomain>
2021-09-08 22:21:55 +0200

    Adds forwards and backwards pass, but with wrong results...

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -3,35 +3,74 @@
 #include <vector>
 #include <fstream>
 #include <cmath>
-#include <iomanip>      // std::setprecision
+#include <iomanip>      // setprecision
+
+using namespace std;
 
 double u(double x); //Declaration
 
 //argc is int and stores number of command-line arguments passed by the user including the name of the program.
 
 int main(int argc, char const *argv[]) {
-    int N = 100;
+
+
+
+    // Problem 2
+    int N_exact = 100;
     double s = 0;
     double x = 0;
     double res = 0;
-    std::vector<double> u_list;
-    std::vector<double> x_list;
-    std::ofstream myfile;
+    vector<double> u_list;
+    vector<double> x_list;
+    ofstream myfile;
     myfile.open ("exact_solution.dat");
-    //myfile << " x  |  u(x) \n";
-    //myfile << "------------\n";
-    for (int i = 0; i <= N; i++){
-        x = double(i)/double(N);
+    for (int i = 0; i <= N_exact; i++){
+        x = double(i)/double(N_exact);
         res = u(x);
         x_list.push_back(x); //Append the value x to x_list.
         u_list.push_back(res);
-        myfile <<std::setprecision(4)  << x_list[i] << " " << u_list[i] << std::endl;
-        std::cout << x << " " << i << " " << x_list[i] << std::endl;
+        myfile <<setprecision(4)  << x_list[i] << " " << u_list[i] << endl;
         }
-    std::cout <<  x_list[0] << std::endl;
-    std::cout << "Hello, World!\n";
+    myfile.close();
 
+    //Problem 7
+    int N = 100;
+    double h = 1.0/double(N);
+    vector<double> a_list;
+    vector<double> b_list;
+    vector<double> c_list;
+    vector<double> g_list;
+    
+    for (int i = 0; i < N; i++){
+        a_list.push_back(-1);
+        b_list.push_back(2);
+        c_list.push_back(-1);
 
+        g_list.push_back(100*exp(-10*(double(i)/double(N)))*h*h);
+    }
+    
+    vector<double> b_tilde;
+    vector<double> g_tilde;
+    b_tilde.push_back(b_list[0]);
+    g_tilde.push_back(g_list[0]);
+    //Forward pass
+    for (int i = 1; i < N; i++){
+        b_tilde.push_back(b_list[i] - (a_list[i])/(b_tilde[i-1]) *c_list[i-1]);
+        g_tilde.push_back(g_list[i] - (a_list[i])/(b_tilde[i-1]) *g_tilde[i-1]);
+    }
+
+    vector<double> v_tilde (N);
+    v_tilde[N-1] = (g_tilde[N-1])/(b_tilde[N-1]);  
+    //Backwards pass
+    for (int i = N-2; i >= 0; i--){
+        v_tilde[i] = (g_tilde[i] - c_list[i] * v_tilde[i+1])/(b_tilde[i]);
+    }
+
+    myfile.open ("solution.dat");
+    for (int i = 0; i <= N; i++){
+        x = double(i)/double(N);
+        myfile <<setprecision(5)  << x << " " << v_tilde[i] << endl;
+        }
     myfile.close();
     return 0;
 }
--- Project1/src/plot.py
+++ Project1/src/plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+#Problem2
 data=np.loadtxt("exact_solution.dat")
 
 x = data[:,0]
@@ -9,3 +9,15 @@ y = data[:,1]
 
 plt.plot(x,y)
 plt.savefig('plot.pdf')
+
+#Problem7
+data=np.loadtxt("solution.dat")
+
+x = data[:,0]
+y = data[:,1]
+
+plt.plot(x,y)
+plt.savefig('solution_plot.pdf')
+
+
+
